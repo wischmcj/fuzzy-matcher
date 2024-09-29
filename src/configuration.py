2065808bf6572b452b23c871b3f6ce839c9c5f63
Collin <cjwischmeyer@gmail.com>
2024-09-29 12:59:49 -0400

    fixing git ignore to allow for actions

--- src/configuration.py
+++ src/configuration.py
@@ -9,7 +9,7 @@ from toml import load
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
 # environment variables
-ROOT_DIR = "src/" if os.environ["FULL_PATH"] == "True" else ""
+ROOT_DIR = "src/" if os.environ.get("FULL_PATH", "False") == "True" else ""
 DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3")
 INPUT_DIR = "".join([ROOT_DIR, os.environ.get("INPUT_DIR", ".")])
 OUTPUT_DIR = "".join([ROOT_DIR, os.environ.get("OUTPUT_DIR", ".")])
@@ -24,7 +24,7 @@ FIELD_ALIAS = CONFIG["field_aliases"]
 # Logging configuration
 logger = logging.getLogger("pmt_fuzzy")
 
-if os.environ["FULL_PATH"] == "True":
+if os.environ.get("FULL_PATH", "False") == "True":
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 # in the case of a local run level =logging.DEBUG
 else:
